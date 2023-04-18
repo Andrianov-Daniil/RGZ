26ec25224176d12b6b9f2ea4f70c93b44afcb2b3
Andrianov-Daniil <99797382+Andrianov-Daniil@users.noreply.github.com>
2023-04-18 23:20:08 +0700

    Add files via upload

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,10 +1,24 @@
 const uuid = require('uuid');
+const path = require('path');
+const {Device} = require('../models/models');
+const ApiError = require('../error/ApiError');
 
 class DeviceController{
     //создание
-    async create (req, res){
-        const { name, proce, brandId, typeId, info} = req.body;
-        const {img} = req.files;
+    async create (req, res, next){
+        try{
+            const { name, proce, brandId, typeId, info} = req.body;
+            const {img} = req.files;
+            let fileName = uuid.v4 + ".jpg";
+            img.mv(path.resolve(__dirname, '..' , 'static', fileName));
+
+            const device = await Device.create({name, price, brand, typeId, img: fileName})
+
+            return res.json(device);
+        }
+        catch(e){
+            next(ApiError.badRequest(e.message));
+        }
     }
 
     //получение
