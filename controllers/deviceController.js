d3775520f31c8c48103960e2a22638c2af502c99
Andrianov-Daniil <99797382+Andrianov-Daniil@users.noreply.github.com>
2023-04-19 18:04:34 +0700

    Add files via upload

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,18 +1,28 @@
 const uuid = require('uuid');
 const path = require('path');
-const {Device} = require('../models/models');
+const {Device, DeviceInfo} = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class DeviceController{
     //создание
     async create (req, res, next){
         try{
-            const { name, proce, brandId, typeId, info} = req.body;
+            let { name, price, brandId, typeId, info} = req.body;
             const {img} = req.files;
-            let fileName = uuid.v4 + ".jpg";
+            let fileName = uuid.v4() + ".jpg";
             img.mv(path.resolve(__dirname, '..' , 'static', fileName));
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});
 
-            const device = await Device.create({name, price, brand, typeId, img: fileName})
+            if(info){
+                info = JSON.parse(info);
+                info.forEach( i => 
+                    DeviceInfo.create({
+                        title: i.title,
+                        descrioption: i.descrioption,  
+                        deviceId: device.id
+                    })
+                )
+            }
 
             return res.json(device);
         }
@@ -23,12 +33,36 @@ class DeviceController{
 
     //получение
     async getAll (req, res){
-        
+        let {brandId, typeId, limit, page} = req.query;
+        page = page || 1;
+        limit = limit || 9;
+        let offset = page * limit - limit;
+        let device;
+        if(!brandId && !typeId){
+            device = await Device.findAndCountAll({limit, offset});
+        }
+        else if(brandId && !typeId){
+            device = await Device.findAndCountAll({where:{brandId}, limit, offset});
+        }
+        else if(!brandId && typeId){
+            device = await Device.findAndCountAll({where:{typeId}, limit, offset});
+        }
+        else{
+            device = await Device.findAndCountAll({where:{brandId, typeId}, limit, offset});
+        }
+        return res.json(device);
     }
 
     //получение 1 
     async getOne(req, res){
-
+        const {id} = req.params;
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            }
+        )
+        return res.json(device);
     }
 }
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,38 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models');
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
 class UserController{
     //регистрация
     async registration (req, res){
+        const {email, password, role} = req.body;
 
+        if(!password){
+            return next(ApiError.badRequest('Введите пароль!'));
+        }
+        if(!email){
+            return next(ApiError.badRequest('Введите email!'));
+        }
+        
+        const candidate = await User.findOne({where: {email}});
+        if (candidate){
+            return ApiError.badRequest('Пользователь с таким email уже существует!');
+        }
+        
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        const basket = await Basket.create({userId: user.id});
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json(token);
     }
 
     //логин
@@ -15,7 +44,7 @@ class UserController{
     async chek (req, res, next){
         const {id} = req.query;
         if (!id){
-            return next(ApiError.badRequest('Не введён id'));
+            return next(ApiError.badRequest('Не введён id!'));
         }
         res.json(id);
     }
