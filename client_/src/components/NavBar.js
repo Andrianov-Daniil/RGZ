a6dbdd841de2d0189a74e283f9b62cc9b328d479
Andrianov-Daniil <99797382+Andrianov-Daniil@users.noreply.github.com>
2023-04-24 13:55:49 +0700

    Add files via upload

--- client_/src/components/NavBar.js
+++ client_/src/components/NavBar.js
@@ -1,25 +1,32 @@
-import React, {useContext} from 'react';
-import {Context} from "../index";
+import React, { useContext } from 'react';
+import { Context } from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import { SHOP_ROUTE } from '../utils/consts';
+import { Button } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 
-const NavBar = () => {
+const NavBar = observer( () => {
     const {user} = useContext(Context);
     return(
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink className = "logo" to={SHOP_ROUTE}><i class='bx bx-building-house'/>Мир квартир</NavLink>
-                <Nav className="ml-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
+                {user.isAuth /*&& user.role == "ADMIN"*/ ?
+                    <Nav className="ml-auto">
+                        <Button variant={'outline-light'} className="button">Админ панель</Button>
+                        <Button variant={'outline-light'} onClick={() => user.setIsAuth(!user.isAuth)}>Выйти</Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto">
+                        <Button variant='outline-light' onClick={() => user.setIsAuth(!user.isAuth)}>Войти</Button>
+                    </Nav>
+            }
             </Container>
         </Navbar>
-    );
-}
+    )
+})
 
 export default NavBar;
