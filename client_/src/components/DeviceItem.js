cb06f7765a6f30ad52f6863d23bbfeff4c1a3440
Andrianov-Daniil <99797382+Andrianov-Daniil@users.noreply.github.com>
2023-04-29 19:32:33 +0700

    Add files via upload

--- client_/src/components/DeviceItem.js
+++ client_/src/components/DeviceItem.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import { useHistory } from 'react-router-dom';
+import { DEVICE_ROUTE } from "../utils/consts";
 
 const DeviceItem = ({device}) => {
     const history = useHistory();
+    
     console.log(history);
     return(
-        <Col md={4} className='col'>
+        <Col md={4} className='col' onClick={() => history.push(DEVICE_ROUTE + '/' + device.id)}>
             <Card className='device'>
                 <Image width={150} height={150} src={device.img} />
                 <div className='mt-2 m-2 d-flex justify-content-between align-items-center'>
--- client_/src/pages/Device.js
+++ client_/src/pages/Device.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 
 const Device = () => {
+    const device = {id: 6, name: '15 pro', price: 110000, rating: 5, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-NepTStjipxyh_gm9vhvJpdmvi4QxaxaTIx4S7jWR9A&s'};
     return(
-        <div>
-            Device
-        </div>
+        <Container className="mt-4">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <line className="d-flex flex-column align-items-center" >
+                        <h2>{device.name}</h2>
+                        <div className=" d-flex align-items-center justify-content-center star"
+                            
+                        >
+                            {device.rating}
+                        </div>
+                    </line>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justidy-content-around"
+                        style={{widht:300, height: 300, fontSize: 32, border: '5px solid lightgrey'}}
+                    >
+
+                        <h3 style={{padding: 60}}>{device.price} Руб.</h3>
+                        <Button variant="outline-dark">Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
